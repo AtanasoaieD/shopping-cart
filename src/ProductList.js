15231e7bd1672476d3a222e886551e5bda9499d0
AtanasoaieD <danatanasoaie17@gmail.com>
2024-12-08 15:26:26 +0200

    add more products

--- src/ProductList.js
+++ src/ProductList.js
@@ -5,6 +5,16 @@ const products = [
   { id: 1, name: "Apple", price: 1.0 },
   { id: 2, name: "Banana", price: 0.5 },
   { id: 3, name: "Orange", price: 0.75 },
+  { id: 4, name: "Mango", price: 1.00},
+  { id: 5, name: "Watermelon", price: 2.00 },
+  { id: 6, name: "Kiwi", price: 0.6 },
+  { id: 7, name: "Pineapple", price: 0.8 },
+  { id: 8, name: "Strawberry", price: 1.00 },
+  { id: 9, name: "Avocado", price: 1.00 },
+  { id: 10, name: "Mango", price: 1.2 },
+  { id: 11, name: "Berries", price: 0.8 },
+  { id: 12, name: "", price: 0.8 },
+  
 ];
 
 
